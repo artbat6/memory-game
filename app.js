89a1c2b0088bb66db94db4a2083667f61efae485
artbat6 <arthurbatesdev@gmail.com>
2021-09-22 17:19:37 -0500

    added animal array to app.js

---
+++ app.js
@@ -0,0 +1,70 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    //card option
+    const cardArray = [
+        {
+            name: 'bird',
+            img: 'images/bird.png'
+        },
+        {
+            name: 'bird',
+            img: 'images/bird.png'
+        },
+        {
+            name: 'butterfly',
+            img: 'images/butterfly.png'
+        },
+        {
+            name: 'butterfly',
+            img: 'images/butterfly.png'
+        },
+        {
+            name: 'fox',
+            img: 'images/fox.png'
+        },
+        {
+            name: 'fox',
+            img: 'images/fox.png'
+        },
+        {
+            name: 'frog',
+            img: 'images/frog.png'
+        },
+        {
+            name: 'frog',
+            img: 'images/frog.png'
+        },
+        {
+            name: 'pig',
+            img: 'images/pig.png'
+        },
+        {
+            name: 'pig',
+            img: 'images/pig.png'
+        },
+        {
+            name: 'skunk',
+            img: 'images/skunk.png'
+        },
+        {
+            name: 'skunk',
+            img: 'images/skunk.png'
+        },
+        {
+            name: 'snail',
+            img: 'images/snail.png'
+        },
+        {
+            name: 'snail',
+            img: 'images/snail.png'
+        },
+        {
+            name: 'spider',
+            img: 'images/spider.png'
+        },
+        {
+            name: 'spider',
+            img: 'images/spider.png'
+        },
+    ]
+})
