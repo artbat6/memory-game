82aaf2668441fd955fcc47d2bfcf17e9421f2d6c
artbat6 <arthurbatesdev@gmail.com>
2021-09-22 18:22:40 -0500

    flip and check for match functions

--- app.js
+++ app.js
@@ -67,7 +67,15 @@ document.addEventListener('DOMContentLoaded', () => {
             img: 'images/spider.png'
         },
     ]
+
+cardArray.sort(() => 0.5 - Math.random)
+
+
 const grid = document.querySelector('.grid')
+const resultDisplay = document.querySelector('#result')
+var cardsChosen = []
+var cardsChosenId = []
+var cardsWon = []
 
 //create your board
 
@@ -76,20 +84,45 @@ function createBoard() {
         var card = document.createElement('img')
         card.setAttribute('src', 'images/dot.png')
         card.setAttribute('data-id', i)
-        //card.addEventListener('click', flipcard)
+        card.addEventListener('click', flipcard)
         grid.appendChild(card)
     }
 }
 // check for matches
-
+function checkForMatch() {
+    var cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenId[0]
+    const optionTwoId = cardsChosenId[1]
+    if (cardsChosen[0] === cardsChosen[1]) {
+        alert('You found a match')
+        cards[optionOneId].setAttribute('src', 'images/white.png')
+        cards[optionTwoId].setAttribute('src', 'images/white.png')
+        cardsWon.push(cardsChosen)
+    } else {
+        cards[optionOneId].setAttribute('src', 'images/dot.png')
+        cards[optionTwoId].setAttribute('src', 'images/dot.png')
+        alert('Sorry, try again!')
+    }
+    cardsChosen = []
+    cardsChosenId = []
+    resultDisplay.textContent = cardsWon.length
+    if (cardsWon.length === cardArray.length/2) {
+        resultDisplay.textContent = 'Congratulations !  You found them all!'
+    }
+}
 
 // flip your card
 function flipCard() {
-    
+    var cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenId.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length === 2) {
+        setTimeout(checkForMatch, 500)
+    }
 }
 
 createBoard() 
-
 
 
 })
