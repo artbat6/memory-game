0b6396d3dcb64da95bfc2f4e8eb8bd8608c3f9a8
artbat6 <arthurbatesdev@gmail.com>
2021-09-22 19:01:09 -0500

    changed empty square color
    also added styling to title

--- app.js
+++ app.js
@@ -101,8 +101,8 @@ function checkForMatch() {
     }
     else if (cardsChosen[0] === cardsChosen[1]) {
       alert('You found a match')
-      cards[optionOneId].setAttribute('src', 'images/white.png')
-      cards[optionTwoId].setAttribute('src', 'images/white.png')
+      cards[optionOneId].setAttribute('src', 'images/pink.png')
+      cards[optionTwoId].setAttribute('src', 'images/pink.png')
       cards[optionOneId].removeEventListener('click', flipCard)
       cards[optionTwoId].removeEventListener('click', flipCard)
       cardsWon.push(cardsChosen)
